2d56bd58c7c9f513ccdb184cd059e2ef054f0455
selman <gunduzgece546@gmail.com>
2022-09-09 01:31:30 +0300

    static files created

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
 
-class Blog(models.Model):
-  title = models.CharField('Başlık',max_length=100)
-  content = models.TextField('Açıklama',max_length=350)
-  avatar = models.ImageField('Resim', blank=True, null=True, upload_to='media/')
-  comment_count = models.IntegerField('Yorum sayısı')
-  view_count = models.IntegerField('Görüntülenme')
-  like_count = models.IntegerField('Beğeni sayısı')
-  publish_date = models.DateField('Yayınlanma tarihi',auto_now_add=True)
+class Profile(models.Model):
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
+  id_user = models.IntegerField()
+  bio = models.TextField(blank=True)
+  profileimg = models.ImageField(upload_to='profile_images',default='blank-profile-picture.png')
+  location = models.CharField(max_length=100,blank=True)
 
   def __str__(self):
-    return f'{self.title}'
-
-  class Meta:
-    ordering = ['publish_date']
+    return self.user.username
